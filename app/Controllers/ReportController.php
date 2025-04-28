2c6c7b66451b268517b5eaea3dc0144b5e43ced8
xxxMichael <mayckolchavez27@gmail.com>
2025-04-27 22:15:12 -0500

    se refactorizo los reportes y sus debidos controllers , se agrego la interfaz ireportservice para interface segregation

---
+++ app/Controllers/ReportController.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once '../app/Services/Report/ReporteGeneralEmpleadoService.php';
+require_once '../app/Services/Report/ReportePorCedulaService.php';
+require_once '../app/Services/Report/ReporteMensualService.php';
+require_once '../app/Services/Report/ReporteSemanalService.php';
+require_once '../app/Database/conexion.php';
+
+class ReportController
+{
+    public function reporteGeneral()
+    {
+        $conexion = new conexion();
+        $reporteService = new ReporteGeneralEmpleadoService($conexion);
+        $reporteService->generate();
+    }
+
+    public function reportePorCedula($cedula)
+    {
+        $conexion = new conexion();
+        $reporteService = new ReportePorCedulaService($conexion);
+        $reporteService->generate(['cedula' => $cedula]);
+    }
+
+    public function reporteSemanal($cedula, $fecha)
+    {
+        $conexion = new conexion();
+        $reporteService = new ReporteSemanalService($conexion);
+        $reporteService->generate([
+            'cedula' => $cedula,
+            'fecha' => $fecha
+        ]);
+    }
+
+    public function reporteMensual($cedula, $mes, $anio)
+    {
+        $conexion = new conexion();
+        $reporteService = new ReporteMensualService($conexion);
+        $reporteService->generate([
+            'cedula' => $cedula,
+            'mes' => $mes,
+            'anio' => $anio
+        ]);
+    }
+}
